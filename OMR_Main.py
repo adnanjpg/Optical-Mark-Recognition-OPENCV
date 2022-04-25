d4ac11f301cbcd25b66b8bcbc4fe5cc44ba75dc4
Adnan <adnanjpg@github.com>
2022-04-25 17:02:55 +0300

    optimization

--- OMR_Main.py
+++ OMR_Main.py
@@ -28,37 +28,42 @@ while True:
     img = cv2.resize(img, (widthImg, heightImg)) 
     imgFinal = img.copy()
 
-    # CREATE A BLANK IMAGE FOR TESTING DEBUGGING IF REQUIRED
+    # np.zeros will create a matrix filled with zeros,
+    # which means a black image.
+    # this will be used to draw the images that will fail
+    # (fall into exception)
     imgBlank = np.zeros((heightImg,widthImg, 3), np.uint8)
 
     # CONVERT IMAGE TO GRAY SCALE
+    # will be put on screen and also used to apply gaussian blur
     imgGray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
     # ADD GAUSSIAN BLUR
+    # will only used to create imgCanny
     imgBlur = cv2.GaussianBlur(imgGray, (5,5), 1) 
 
     # APPLY CANNY 
     imgCanny = cv2.Canny(imgBlur,10,70)
 
-    try:
-        ## FIND ALL COUNTOURS
+    # will be only used to display
+    imgContours = img.copy() 
+    
+    # FIND ALL CONTOURS
+    contours, hierarchy = cv2.findContours(imgCanny, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE) 
 
-        # COPY IMAGE FOR DISPLAY PURPOSES
-        imgContours = img.copy() 
+    # DRAW ALL DETECTED CONTOURS
+    cv2.drawContours(imgContours, contours, -1, (0, 255, 0), 10)
+
+    try:
+ 
         # COPY IMAGE FOR DISPLAY PURPOSES
         imgBigContour = img.copy() 
-
-        # FIND ALL CONTOURS
-        contours, hierarchy = cv2.findContours(imgCanny, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE) 
-
-        # DRAW ALL DETECTED CONTOURS
-        cv2.drawContours(imgContours, contours, -1, (0, 255, 0), 10) 
 
         # FILTER FOR RECTANGLE CONTOURS
         rectCon = utils.rectContour(contours) 
 
         # GET CORNER POINTS OF THE BIGGEST RECTANGLE
-        biggestPoints= utils.getCornerPoints(rectCon[0]) 
+        biggestPoints = utils.getCornerPoints(rectCon[0]) 
 
         # GET CORNER POINTS OF THE SECOND BIGGEST RECTANGLE
         gradePoints = utils.getCornerPoints(rectCon[1]) 
@@ -66,7 +71,7 @@ while True:
         if biggestPoints.size != 0 and gradePoints.size != 0:
 
             # BIGGEST RECTANGLE WARPING
-            biggestPoints=utils.reorder(biggestPoints) # REORDER FOR WARPING
+            biggestPoints = utils.reorder(biggestPoints) # REORDER FOR WARPING
             cv2.drawContours(imgBigContour, biggestPoints, -1, (0, 255, 0), 20) # DRAW THE BIGGEST CONTOUR
             pts1 = np.float32(biggestPoints) # PREPARE POINTS FOR WARP
             pts2 = np.float32([[0, 0],[widthImg, 0], [0, heightImg],[widthImg, heightImg]]) # PREPARE POINTS FOR WARP
@@ -86,19 +91,19 @@ while True:
             imgThresh = cv2.threshold(imgWarpGray, 170, 255,cv2.THRESH_BINARY_INV )[1] # APPLY THRESHOLD AND INVERSE
 
             boxes = utils.splitBoxes(imgThresh) # GET INDIVIDUAL BOXES
-            # cv2.imshow("Split Test ", boxes[3])
-            countR=0
-            countC=0
+            cv2.imshow("Split Test ", boxes[3])
+            countR = 0
+            countC = 0
             myPixelVal = np.zeros((questions,choices)) # TO STORE THE NON ZERO VALUES OF EACH BOX
             for image in boxes:
                 #cv2.imshow(str(countR)+str(countC),image)
                 totalPixels = cv2.countNonZero(image)
                 myPixelVal[countR][countC]= totalPixels
                 countC += 1
-                if (countC==choices):countC=0;countR +=1
+                if (countC==choices): countC = 0; countR += 1
 
             # FIND THE USER ANSWERS AND PUT THEM IN A LIST
-            myIndex=[]
+            myIndex = []
             for x in range (0,questions):
                 arr = myPixelVal[x]
                 myIndexVal = np.where(arr == np.amax(arr))
@@ -106,14 +111,13 @@ while True:
             #print("USER ANSWERS",myIndex)
 
             # COMPARE THE VALUES TO FIND THE CORRECT ANSWERS
-            grading=[]
+            grading = []
             for x in range(0,questions):
                 if ans[x] == myIndex[x]:
                     grading.append(1)
                 else:grading.append(0)
-            #print("GRADING",grading)
+
             score = (sum(grading)/questions)*100 # FINAL GRADE
-            #print("SCORE",score)
 
             # DISPLAYING ANSWERS
             utils.showAnswers(imgWarpColored,myIndex,grading,ans) # DRAW DETECTED ANSWERS
